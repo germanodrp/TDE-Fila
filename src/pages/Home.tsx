2ab6d8df7517fc9336504b14667254b415c77e9e
Guilherme de Alcantara Machado <guilherme.machado@zema.com>
2022-09-26 18:49:58 -0300

    Adicionando uns trem

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,88 @@
 import { useState } from "react";
 import { ButtonGeneratePassword } from "../components/ButtonGeneratePassword";
 
+
+interface addToQueue {
+    password: string;
+    typePassword: string;
+}
+
 export function Home() {
 
-    const [showMessage, setShowMessage] = useState<boolean>(false)
+    const [numberPreferred, setNumberPreferred] = useState<number>(1)
+    const [preferredPasswordQueue, setPreferredPasswordQueue] = useState<string[]>([])
 
-    function handleGeneratePassword() {
-        setShowMessage(!showMessage)
+    const [numberNormal, setNumberNormal] = useState<number>(1)
+    const [normalPasswordQueue, setNormalPasswordQueue] = useState<string[]>([])
+
+
+
+    function handleGeneratePreferredPassword() {
+
+        setNumberPreferred(numberPreferred + 1)
+        const preferedPassword = 'CXP-' + numberPreferred
+        addToQueue({
+            password: preferedPassword,
+            typePassword: 'Preferencial'
+        })
+
+    }
+
+    function handleGenerateNormalPassword() {
+
+        setNumberNormal(numberNormal + 1)
+        const normalPassword = 'CXN-' + numberNormal
+        addToQueue({
+            password: normalPassword,
+            typePassword: 'Normal'
+        })
+
+    }
+
+    function addToQueue({ typePassword, password }: addToQueue) {
+        if (typePassword === 'Preferencial') {
+            setPreferredPasswordQueue([...preferredPasswordQueue, password])
+        }
+        else {
+            setNormalPasswordQueue([...normalPasswordQueue, password])
+        }
+
     }
 
     return (
         <div>
             <div className="justify-center pt-20 gap-2 flex mt-5">
-                <ButtonGeneratePassword description="Gerar senha para fila com prioridade" onClick={handleGeneratePassword} />
-                <ButtonGeneratePassword description="Gerar senha para fila sem prioridade" onClick={handleGeneratePassword} />
+                <ButtonGeneratePassword
+                    description="Gerar senha para fila sem prioridade"
+                    onClick={handleGenerateNormalPassword}
+                />
+                <ButtonGeneratePassword
+                    description="Gerar senha para fila com prioridade"
+                    onClick={handleGeneratePreferredPassword}
+                />
             </div>
 
-            <div className="justify-center items-center flex mt-5">
-                <h1 className="text-white text-lg">
-                    {showMessage ? 'TOMA JEFIN E GERMANO' : null}
-                </h1>
+            <div className="justify-center flex mt-5 gap-52">
+                <table className="flex mt-8">
+                    <tbody className="text-white border-collapse border-spacing-y-52">Senhas normais:
+                        {normalPasswordQueue.map(password => (
+                            <tr key={password}>
+                                <td>{password}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                <table className="flex mt-8">
+                    <tbody className="text-white border-collapse border-spacing-y-52">Senhas preferencias:
+                        {preferredPasswordQueue.map(password => (
+                            <tr key={password}>
+                                <td>{password}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
             </div>
         </div>
 
